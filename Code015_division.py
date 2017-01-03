bf9d45259f5f5cbfd2d158398db5b4115b2fd89a
aayush-bhardwaj <aayush.bhj@gmail.com>
2017-01-03 12:46:00 +0530

    Code015

--- Code015_division.py
+++ Code015_division.py
@@ -1,9 +1,11 @@
+from __future__ import division
 #DAY07_24DEC2016_Code015.py
 
 #NOTES
 
 '''
-
+Using __future__ we can import the features of higher python versions 
+without upgrading our python .
 '''
 
 #QUES
@@ -13,9 +15,14 @@
 '''
 
 #SOLUTION
+def division(a,b):
+	#Using __future__ I am using divsion as in Python 3.x
+	print a/b #7/3 = 2.3333
+	print a//b #7/3 = 2
 
 
 if __name__ == '__main__':
-    s = raw_input()
-    result = swap_case(s)
+    a = int(raw_input())
+    b = int(raw_input())
+    result = division(a,b)
     print result
